87a7080e9f550854757bd491495d89c5cada9ce4
wikipediamaster <yusuftutorialchanel@gmail.com>
2024-06-11 01:42:50 +0800

    update commit

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 # echo "# wiki" >> README.md
-git init
+# git init
 git add *
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/wikipediamaster/wiki.git
+git commit -m "update commit"
+# git branch -M main
+# git remote add origin https://github.com/wikipediamaster/wiki.git
 git push -u origin main
